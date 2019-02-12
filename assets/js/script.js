79d92cc19f402bcbe2a1360f7b4a25c352f36682
notmsc <mscaro22@gmail.com>
2019-02-12 02:57:14 -0800

    added UI however not yet mobile responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,66 @@
 //Navbar Initializer
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-});
-//END: Navbar initializer
+    //END: Navbar initializer
 
-var ariel = document.getElementById('ariel');
-var happen = document.getElementById('happen');
-var shiba = document.getElementById('shiba');
+    var $section1 = $('#section1');
+    var $section2 = $('#section2');
+    var $section3 = $('#section3');
+    var $portfolio = $('#portfolio-container');
+    var isJump = false;
 
-var $myIntro = $('#myIntro');
-var $myDesign = $('#myDesign');
-var $shiba = $('#shiba');
-var $section1 = $('#section1');
-var $section2 = $('#section2');
-
-
-var myFullpage = new fullpage('#fullpage', {
-    // anchors: ['firstPage', 'secondPage', '3rdPage'],
-    menu: '#menu',
-    loopTop: true,
-    loopBottom: true,
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['About', 'Designs', 'Contact'],
-    onLeave: function(origin, destination, direction) {
-        if(origin.index == 0 && direction == 'down') {
-            //animate first page down
-            $myIntro.removeClass('fadeInLeft');
-            $myDesign.addClass('fadeInLeft');
-            $section2.addClass('fadeInRight');
-        } else if (origin.index == 0 && direction == 'up') {
-            //animate first page up
-            $myIntro.removeClass('fadeInLeft');
-            $shiba.addClass('fadeInLeft');
-        } else if(origin.index == 1 && direction == 'down') {
-            //animate second page down
-            $myDesign.removeClass('fadeInLeft');
-            $shiba.addClass('fadeInLeft');
-        } else if (origin.index == 1 && direction == 'up') {
-            //animate second page up
-            $myDesign.removeClass('fadeInLeft');
-            $myIntro.addClass('fadeInLeft');    
-        } else if (origin.index == 2 && direction == 'down') {
-            //animate third page down
-            $shiba.removeClass('slideInLeft');
-            $myIntro.addClass('fadeInLeft');
-        } else if (origin.index == 2 && direction == 'up') {
-            //animate third page up
-            $shiba.removeClass('slideInLeft');
-            $myDesign.addClass('fadeInLeft');
+    function jumper() {
+        if (isJump == true) {
+            setInterval(function() {
+                var bounce = 'animated bounce';
+                var endAnimate = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+                $('.custom-buttons').addClass(bounce).one(endAnimate, function() {
+                    $('.custom-buttons').removeClass(bounce);
+                });
+            }, 10 * 1000);
+        } else if (isJump == false) {
+            return;
         }
     }
+
+    var myFullpage = new fullpage('#fullpage', {
+        anchors: ['firstPage', 'secondPage', 'thirdPage'],
+        menu: '#menu',
+        loopTop: true,
+        loopBottom: true,
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['About', 'Designs', 'Contact'],
+        onLeave: function (origin, destination, direction) {
+            if (origin.index == 0 && direction == 'down') {
+                //animate first page down
+                $section2.addClass('fadeInRight');
+                $portfolio.addClass('fadeInLeft');
+                isJump = true;
+                jumper();
+            } else if (origin.index == 0 && direction == 'up') {
+                //animate first page up
+                $section3.addClass('fadeIn');
+                isJump = true;
+                jumper();
+            } else if (origin.index == 1 && direction == 'down') {
+                //animate second page down
+                $section3.addClass('fadeIn');
+                isJump = true;
+                jumper();
+            } else if (origin.index == 1 && direction == 'up') {
+                //animate second page up  
+            } else if (origin.index == 2 && direction == 'down') {
+                //animate third page down
+                isJump = false;
+                jumper();
+            } else if (origin.index == 2 && direction == 'up') {
+                //animate third page up
+                $section2.addClass('fadeInRight');
+                $portfolio.addClass('fadeInLeft');
+                isJump = false;
+                jumper();
+            }
+        }
+    });
 });
