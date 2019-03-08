f2346fb200155c8a2ad1731cf56f1c9493ec4993
notmsc <mscaro22@gmail.com>
2019-03-08 15:24:37 -0800

    still need to create icons, and set up modals

---
+++ assets/js/portfolio.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    //side nav
+    $('.sidenav').sidenav();
+    //end side nav
+    
+    //materialize carousel
+    $(".carousel").carousel({
+        fullWidth: true,
+        indicators: true,
+    });
+    //end materialize carousel
+
+});
